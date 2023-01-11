744f185c633c76d626892f743c11b51d52d066be
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-10 20:02:00 -0700

    but for real

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -4,11 +4,7 @@ import classes from './Zodiac.module.css';
 export default function Zodiac(props) {
   return (
     <div className={classes.zodiac}>
-      <img
-        className={classes.image}
-        alt={props.name}
-        src={`${process.env.PUBLIC_URL}/zodiacs/${props.name}.png`}
-      />
+      <img className={classes.image} alt={props.name} src={`zodiacs/${props.name}.png`} />
       <span className={classes.dates}>{props.dates}</span>
       <span className={classes.name}>{props.name}</span>
     </div>
